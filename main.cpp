bc8bad4df82ea7bacc7fbdd8fb50ce5764296b0d
joseph-mbote <56190345+joseph-mbote@users.noreply.github.com>
2019-11-09 01:17:30 +0300

    Add files via upload
    program of solving a quadratic function

--- main.cpp
+++ main.cpp
@@ -1,29 +1,40 @@
-// C++ recursive function to
-// solve tower of hanoi puzzle
-#include <bits/stdc++.h>
+#include <iostream>
+#include<math.h>
 using namespace std;
 
-void towerOfHanoi(int n, char from_rod,
-                    char to_rod, char aux_rod)
-{
-    if (n == 1)
-    {
-        cout << "Move disk 1 from rod " << from_rod <<
-                            " to rod " << to_rod<<endl;
-        return;
-    }
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-    cout << "Move disk " << n << " from rod " << from_rod <<
-                                " to rod " << to_rod << endl;
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
-}
-
-// Driver code
 int main()
 {
-    int n = 4; // Number of disks
-    towerOfHanoi(n, 'A', 'C', 'B'); // A, B and C are names of rods
-    return 0;
-}
 
-// This is code is contributed by rathbhupendra
+float a,b,c,sq,sq2,h,i;
+float x1,x2,x3,x4,sqt,sqt2;
+cout<<"Quadratic Equation is in form of ax^2+bx+c=0\n\n";
+cout<<"Enter coefficient of a\n";
+cin>>a;
+cout<<"Enter coefficient of b\n";
+cin>>b;
+cout<<"Enter coefficient of c\n";
+cin>>c;
+sq=(b*b)-(4*a*c);
+sqt=sqrt(sq);
+/*discriminant solving i.e b^2-4ac*/
+x1=(-b+sqt)/(2*a);
+x2=(-b-sqt)/(2*a);/*for b^2-4ac>0 and for b^2-4ac=0*/
+sq2=-1*sq;
+sqt2=sqrt(sq2);
+x3=(-b)/(2*a);
+i=sqt2/(2*a);
+x4=(-b)/(2*a);/*for b^2-4ac<0*/
+cout<<"The square of the roots is: "<<sq<<endl;
+h=sqt2/(2*a);
+{
+if(sq>0)
+cout<<"The roots are Real and Distinct\nThe Square root is: "<<sqt<<"\n x is either : "<<x1<<" or" << x2<<endl;
+else if(sq<0)
+cout<<"The roots are Complex (i.e Real and Imaginary)\nThe Square root is: "<<sqt2<<"i\nx is either : "<<x3<<" or "<< i <<" or "<<x4<<" or " <<h <<endl;
+else
+cout<<"The roots are Real and Repeated\nThe Square root is: "<<sq<<"\n x is : "<<x1<<endl;
+}
+return 0;
+
+
+}
